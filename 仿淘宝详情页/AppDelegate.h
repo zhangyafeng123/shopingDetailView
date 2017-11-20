3ffaa6a1c2bde5c035a9ac1033f1af0e95f57751
zhangyafeng123 <1240334911@qq.com>
2017-11-20 11:28:33 +0800

    Initial Commit

---
+++ 仿淘宝详情页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿淘宝详情页
+//
+//  Created by linjianguo on 2017/11/20.
+//  Copyright © 2017年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿淘宝详情页/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿淘宝详情页
+//
+//  Created by linjianguo on 2017/11/20.
+//  Copyright © 2017年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
